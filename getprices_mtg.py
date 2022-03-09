2254d395cf5a948cbcb9ae48fb279ede95b8e41e
Mark Taylor <taylor26@seattleu.edu>
2022-03-08 17:32:25 -0800

    Initial commit

---
+++ getprices_mtg.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
+                         '537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
+
+cards_NeonDynasty2022_mythic = [
+    'Ao+the+Dawn+Sky', 'Atsushi+the+Blazing+Sky', 'Blade+of+the+Oni',
+    'Explosive+Singularity', 'Hidetsugu+Consumes+All', 'Jin-Gitaxias+Progress+Tyrant',
+    'Jugan+Defends+The+Temple', 'Junji+the+Midnight+Sky', 'Kairi+the+Swirling+Sky',
+
+    ]
+
+for i in cards_NeonDynasty2022_mythic:
+
+    url = 'https://www.mtggoldfish.com/price/Kamigawa+Neon+Dynasty/' + i
+    #url = 'https://www.mtggoldfish.com/price/Kamigawa+Neon+Dynasty/Ao+the+Dawn+Sky#paper'
+
+    r = requests.get(url)
+
+    #print(r.text)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    #print(soup.title.text)
+
+
+    #cardName = soup.find('h3', {'class':'gatherer-name'}).text
+    price = soup.find('div', {'class':'price-box-price'}).text
+    print(i)
+    print(price)
